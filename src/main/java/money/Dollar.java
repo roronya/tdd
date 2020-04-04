941fd6616e123f8c89a2762e4e6b9f6c53f90be8
roronya <roronya628@gmail.com>
2020-04-04 14:58:11 +0900

    第2章

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -8,7 +8,8 @@ class Dollar {
     this.amount = amount;
   }
 
-  void times(int multiplier) {
-    amount = amount * multiplier;
+  Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
   }
+
 }
