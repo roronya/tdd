bd2136372de5fd25a27ae04aa77d4c122b16e8a3
roronya <roronya628@gmail.com>
2020-04-04 15:07:55 +0900

    第3章

---
+++ src/test/java/money/MoneyTest.java
@@ -0,0 +1,25 @@
+package money;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+  }
+}
